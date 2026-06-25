e3588e0f927e1d936013ab430b9f7a1fa7a77802
Amey-1511 <2024eeb1180@iitrpr.ac.in>
2026-06-25 14:57:33 +0530

    Create tb_seq_detector_moore.v

---
+++ tb_seq_detector_moore.v
@@ -0,0 +1,54 @@
+module tb_seq_detector_moore;
+
+reg clk;
+reg rst;
+reg din;
+
+wire detect;
+
+seq_detector_1011_moore DUT (
+    .clk(clk),
+    .rst(rst),
+    .din(din),
+    .detect(detect)
+);
+
+always #5 clk = ~clk;
+
+task send_bit;
+    input bit_val;
+    begin
+        @(posedge clk);
+        din = bit_val;
+    end
+endtask
+
+initial begin
+
+    clk = 0;
+    rst = 1;
+    din = 0;
+
+    #20 rst = 0;
+
+    send_bit(1);
+    send_bit(0);
+    send_bit(1);
+    send_bit(1);
+
+    send_bit(0);
+    send_bit(1);
+    send_bit(0);
+    send_bit(1);
+    send_bit(1);
+
+    #50;
+    $finish;
+end
+
+initial begin
+    $monitor("Time=%0t Din=%b Detect=%b",
+              $time,din,detect);
+end
+
+endmodule
